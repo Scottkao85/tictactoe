a3f7254798506fc68e2c187e459b5edbbb6ef958
scott kao <scott_kao@apple.com>
2017-11-25 15:10:32 -0800

    TASK: progress in app. ability to set state and props in Square and Board

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import './index.css';
 
 
 class Square extends React.Component {
+
   render() {
+    // TODO: use onClick={this.props.onClick}
+    // TODO: replace this.state.value with this.props.value
     return (
-      <button className="square">
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -14,10 +17,28 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick= {() => this.handleClick(i)}
+      />
+    );
+  }
+  
   render() {
     const status = 'Next player: X';
 
@@ -25,19 +46,13 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
         </div>
       </div>
     );
